36a7c1f3609f143f31e4fa9a8da3010168ec069a
jadegoncalves <31525542+jadegoncalves@users.noreply.github.com>
2021-01-10 18:37:44 -0300

    visualização dos dados melhorada

--- simulacao.py
+++ simulacao.py
@@ -2,16 +2,21 @@ import random
 import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import statistics
 
 investimento_inicial = 10000.00     # qual investimento inicial na empresa
-porcento_investimento = 0           # quanto da margem vai investir (porcentagem)
+porcento_investimento = 30          # quanto da margem vai investir (porcentagem)
 salario_inicial = 1000.00           # quanto cada funcionario irá receber de salário inicialmente
 sub_custo_fixo = 2000.00            # custo fixo de luz, água, condomínio
 custo_unitario = 100.00             # quanto custa produzir UM produto
-demanda = 100                       # quantas peças eu tenho que produzir mensalmente
+demanda = 200                       # quantas peças serão produzidas mensalmente
+cadencia_por_pessoa = 50            # quantas peças cada pessoa consegue produzir
 margem = 200                        # quantos % do custo para preço de venda
-carteira_inicial = 0.01               # com quantos reais a empresa irá começar
-probabilidade_satisfacao = 60       # probabilidade de satisfação dos funcionários em relação a empresa
+carteira_inicial = 0.01             # com quantos reais a empresa irá começar
+probabilidade_satisfacao = 70       # probabilidade de satisfação dos funcionários em relação a empresa
+probabilidade_produtividade = 90    # probabilidade de contratação de alguém apto
+probabilidade_altera_demanda = 20   # probabilidade de aumentar ou diminuir a demanda da produção
+periodo_simulacao = 20              # tempo de simulacao em anos
 
 class Empresa:
 
@@ -24,11 +29,20 @@ class Empresa:
         self.periodo = []
         self.mes = 1 # TEMPORARIO RETIRAR
 
-    def contrataFuncionario(self):
-        quantidade = 5  
+    def alteraStatusFuncionariosMassa(self, contrata = True):
 
-        for p in range(0, quantidade):
+        i = len(self.dicionarioFuncionarios)
+        quantidade = round(demanda / cadencia_por_pessoa)
+
+        while (i < quantidade):
             self.dicionarioFuncionarios.append(Funcionario(salario_inicial))
+            i += 1
+      
+        while (i > quantidade):
+            rand = random.randint(0, len(self.dicionarioFuncionarios) - 1)
+            self.dicionarioFuncionarios.pop(rand)
+            i -= 1
+
 
     def calculaGastos(self):
         produtividade_total = 1
@@ -71,6 +85,9 @@ class Empresa:
             self.periodo.append(self.mes)
             self.receitas.append(self.carteiraTotal)
             self.updateFuncionarios("mensal")
+            if(self.alteraDemanda()):
+                self.alteraStatusFuncionariosMassa()
+
             self.mes += 1
     
     def updateFuncionarios(self, time = "mensal"):
@@ -84,7 +101,37 @@ class Empresa:
                     self.demiteFuncionario(funcionario)
 
     def demiteFuncionario(self, funcionario):
+
         self.dicionarioFuncionarios.remove(funcionario)
+        self.alteraStatusFuncionariosMassa()
+        
+    def alteraDemanda(self):
+        demandaAlterada = False
+        global demanda
+        desvioPadraoEntregas = statistics.pvariance(self.receitas)
+
+        # Aumento por boas práticas
+        if(desvioPadraoEntregas < (0.1*demanda)):
+            rand = random.randint(10,100)
+            demanda = round(demanda * (1 + (rand/100)))
+            demandaAlterada = True
+        else:
+            # o que posso fazer se não atinjo a meta... hum..
+            pass
+        
+        # Aumento por probabilidade
+        rand = random.randint(0,100)
+        if(rand >= (100 - probabilidade_altera_demanda)):
+            randAlteracao = random.randint(2,6)
+
+            if (rand >= 5):
+                demanda = round(demanda * (1 + (randAlteracao/100)))
+            else:
+                demanda = round(demanda * (1 - (randAlteracao/100)))
+            
+            demandaAlterada = True
+
+        return demandaAlterada
 
     def reset(self):
         self.carteira = 0.01
@@ -112,14 +159,21 @@ class Funcionario:
 
     def __init__(self, salario):
         self.salario = salario_inicial
-        self.produtividade = 100
-        self.idade = 18
-        self.satisfacao = 50
+
         rand = random.randint(0, 100)
+
         if rand < 60:
             self.tipo = "Direto"
         else:
             self.tipo = "Indireto"
+
+        if(rand >= (100 - probabilidade_produtividade)):
+            self.produtividade = 100
+        else:
+            self.produtividade = rand + 10
+
+        self.idade = 18
+        self.satisfacao = 100
 
     def aumentaSalario(self):
         if(self.idade % 5 == 0):
@@ -135,9 +189,11 @@ class Funcionario:
 
     def modificaSatisfacao(self, aumenta):
         if(aumenta == 1):
-            self.satisfacao += 10
+            if self.satisfacao < 100:
+                self.satisfacao += 1
         else:
-            self.satisfacao -= 10
+            if self.satisfacao > 0:
+                self.satisfacao -= 1
 
         if(self.tipo == "Direto"):
             self.modificaProdutividade(aumenta)
@@ -196,27 +252,26 @@ class Grafico:
         ax.set_ylim(min(self.dados.receitas), max(self.dados.receitas))
 
         ani = animation.FuncAnimation(fig, self.animate,
-        interval=40, blit=True, repeat= False, frames=(len(self.dados.receitas)-1))
+        interval=20, blit=True, repeat= False, frames=(len(self.dados.receitas)-1))
 
         plt.show()  # melhorar visualização do grafico
 
 if __name__ == "__main__":
 
     simulaEmpresa = Empresa()
-    simulaEmpresa.contrataFuncionario()
+    simulaEmpresa.alteraStatusFuncionariosMassa()
     arq = open('log.txt', 'w')
     escreverLog = ""
 
-    for ano in range(0, 10):
+    for ano in range(0, periodo_simulacao):
 
-        print("ANO " + str(ano))
         inicial = simulaEmpresa.carteiraTotal
 
         simulaEmpresa.updateAnual()
         simulaEmpresa.updateFuncionarios("anual")
 
         final = simulaEmpresa.carteiraTotal
-        crescimento = "%.2f" % (((final - inicial)/inicial) *100)
+        crescimento = "%.2f" % (((final - inicial)/abs(inicial)) *100)
         print("Crescimento de: " + crescimento + "%")
         escreverLog = escreverLog + "Crescimento de: " + crescimento + "%\r\n"        
     
